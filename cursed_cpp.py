8e396e1de31106ba628e9a7bccac74899b5401ee
Codebender <64493808+Codebenderr@users.noreply.github.com>
2023-01-22 16:54:36 +0300

    Add files via upload

--- cursed_cpp.py
+++ cursed_cpp.py
@@ -1,7 +1,8 @@
 import sys
 
 
-def tab_cnt(txt: str):
+
+def tab_cnt(txt: str) -> str:
 	"""Count the number of prefix tabs"""
 	
 	count = 0
@@ -16,36 +17,38 @@ def tab_cnt(txt: str):
 
 
 
-
-def is_empty(txt: str):
+def is_empty(txt: str) -> str:
 	"""Check whether a string consists of whitespaces and tabs only"""
 
 	return (txt.replace('\t', '') + ' ').isspace()
 
 
 
-def format_closing_braces(tab: int, n = 1):
+def format_closing_braces(tab: int, n = 1) -> str:
 	"""Format descending '}' characters starting from 'tab'"""
 
 	_str = ''
+
 	for i in range(n):
 		_str += (tab - i)*'\t' + '}\n'
+
 	return _str
 
 
 
-def format_opening_braces(tab: int, n = 1):
+def format_opening_braces(tab: int, n = 1) -> str:
 	"""Format ascending '{' characters starting from 'tab'"""
 
 	_str = ''
+
 	for i in range(n):
 		_str += (tab + i)*'\t' + '{\n'
+
 	return _str
 
 
 
-
-def uncurse(txt: str):
+def uncurse(txt: str) -> str:
 	"""Convert a pythonic CPP source into a regular CPP source"""
 
 
@@ -53,7 +56,9 @@ def uncurse(txt: str):
 	lines = [line for line in lines if not is_empty(line)]
 	lines.append('')
 
+
 	tab_cnts = [tab_cnt(line) for line in lines]
+
 
 	final = ''
 
@@ -80,7 +85,6 @@ def uncurse(txt: str):
 		"""
 
 
-
 		diff = tab_cnts[i+1] - tab_cnts[i]
 
 		if diff > 0:
@@ -97,12 +101,12 @@ def uncurse(txt: str):
 
 
 
-
 if __name__ == '__main__':
 	if len(sys.argv) > 1:
 		with open(sys.argv[1], 'r') as f:
 			with open('cursed_' + sys.argv[1], 'w') as f2:
 				f2.write(uncurse(f.read()))
+
 	elif len(sys.argv) > 2:
 		with open(sys.argv[1], 'r') as f:
 			with open(sys.argv[2], 'w') as f2:
