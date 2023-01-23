88165463b1be980bbf3ccadf45b62bc026704806
Mert-Akar <123328879+Mert-Akar@users.noreply.github.com>
2023-01-23 19:55:24 +0300

    Update cursed_cpp_compile.py

--- cursed_cpp_compile.py
+++ cursed_cpp_compile.py
@@ -9,6 +9,8 @@ if argCount == 3:
             f2.write(uncurse(f.read()))
             compiler = sys.argv[2]
             os.system(f"{compiler} {sys.argv[1]}")
+            
+            
 elif argCount == 4:
     with open(sys.argv[1], 'r') as f:
         with open(sys.argv[2], 'w') as f2:
