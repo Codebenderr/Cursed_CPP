2abcbe5cbf0b4f3487ec7db4396959ecc4e97c8d
Mert-Akar <123328879+Mert-Akar@users.noreply.github.com>
2023-01-23 20:01:01 +0300

    Update cursed_cpp_compile.py

--- cursed_cpp_compile.py
+++ cursed_cpp_compile.py
@@ -6,7 +6,7 @@ argCount = len(sys.argv)
 if argCount == 3:
     with open(sys.argv[1], 'r') as f:
         with open('uncursed_' + sys.argv[1], 'w') as f2:
-            f2.write(uncurse(f.read()))
+            f2.write(cursed_cpp.uncurse(f.read()))
             compiler = sys.argv[2]
             os.system(f"{compiler} {sys.argv[1]}")
             
@@ -14,7 +14,7 @@ if argCount == 3:
 elif argCount == 4:
     with open(sys.argv[1], 'r') as f:
         with open(sys.argv[2], 'w') as f2:
-            f2.write(uncurse(f.read()))
+            f2.write(cursed_cpp.uncurse(f.read()))
             compiler = sys.argv[3]
             os.system(f"{compiler} {sys.argv[2]}")
     
