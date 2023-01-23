2438e78296699b8150159be1d54a0ba59e104a71
Codebender <64493808+Codebenderr@users.noreply.github.com>
2023-01-23 22:55:45 +0300

    Add files via upload

--- ccpp.c
+++ ccpp.c
@@ -1,10 +1,14 @@
+/*
+// This exists so that 'cursed_cpp.py' can be run without typing 'python cursed_cpp.py' all the time
+*/
+
 #include <stdlib.h>
 #include <string.h>
 
 int main(int argc, char* argv[]) {
 
-	char final[256];
-	int n = 0;
+	char final[256] = "python cursed_cpp.py ";
+	int n = 21;
 
 	for (int i = 1; i < argc; i++)
 	{
